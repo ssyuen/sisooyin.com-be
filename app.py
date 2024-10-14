aaa6150f20b50b1bc65ee8bfd3f685d9bb3a39e8
ssyuen <samuel.s.yuen@gmail.com>
2024-10-14 09:50:31 -0400

    added CORS support

--- app.py
+++ app.py
@@ -1,26 +1,20 @@
 from flask import Flask, jsonify, request
 from modules.peloton_wrapper import PelotonClient
 import os
+from flask_cors import CORS
 from json import loads, dumps
 
 
 app = Flask(__name__)
-
-
-# Define a route for the API endpoint
-@app.route("/api/data", methods=["GET"])
-def get_data():
-    # Example data to return
-    data = {"message": "Hello, API!", "status": "success"}
-    return jsonify(data)
+CORS(app)
 
 
 @app.route("/api/workouts", methods=["GET"])
 def get_workouts():
     client = PelotonClient(username=os.environ["P_EMAIL"], password=os.environ["P_PW"])
     workouts = client.fetch_workouts()
-    
-    return jsonify(workouts)
+    weekly_workouts = client.accumulate_work_and_distance(workouts)
+    return jsonify(weekly_workouts)
 
 
 # Run the Flask app
